8c3d36eeee9bdd4e2596e6e7f73463a0149cf9ee
Dhruv_Choudhary <dhruv.235917101@vcet.edu.in>
2024-10-02 14:30:49 +0530

    Dowhileloop.java

---
+++ EXP1/Dowhileloop.java
@@ -0,0 +1,16 @@
+class Dowhileloop
+{
+  public static void main(String arg[])
+  {
+    int a=0;
+    do
+    {
+      if(a%20==0)
+      {
+        System.out.println(a);
+      }
+      a++;
+    }
+      while(a<=100);
+  }  
+}
